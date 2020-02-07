54ff4b7f52b047d9ed5c0af2e468294f0f2f176a
Izzy Swart <zenerboson@gmail.com>
2020-02-07 12:21:16 -0800

    abstract bitbuf traits

--- src/lib.rs
+++ src/lib.rs
@@ -15,18 +15,31 @@ impl From<Insufficient> for CopyError {
     }
 }
 
-#[derive(Debug)]
-pub struct BitBuf<'a> {
-    data: &'a [u8],
-    prefix: u8,
+pub trait BitBuf {
+    fn advance(&mut self, bits: usize) -> Result<(), Insufficient>;
+    fn copy_to_slice(&mut self, dst: &mut [u8], bits: usize) -> Result<(), CopyError>;
+    fn copy_aligned(&mut self, dst: &mut [u8]) -> Result<(), CopyError> {
+        self.copy_to_slice(dst, dst.len() * 8)
+    }
+    fn pop(&mut self) -> Option<bool>;
+    fn pop_byte(&mut self) -> Option<u8> {
+        let mut data = [0];
+        self.copy_aligned(&mut data).ok()?;
+        Some(data[0])
+    }
+    fn remaining(&self) -> usize;
+    fn len(&self) -> usize;
 }
 
-impl<'a> BitBuf<'a> {
-    pub fn new(data: &'a [u8]) -> Self {
-        BitBuf { data, prefix: 0 }
-    }
+#[derive(Debug)]
+pub struct BitSlice<'a> {
+    data: &'a [u8],
+    prefix: u8,
+    len: usize,
+}
 
-    pub fn advance(&mut self, bits: usize) -> Result<(), Insufficient> {
+impl<'a> BitBuf for BitSlice<'a> {
+    fn advance(&mut self, bits: usize) -> Result<(), Insufficient> {
         self.prefix += (bits & 7) as u8;
         if self.prefix >= 8 {
             self.prefix -= 8;
@@ -34,48 +47,21 @@ impl<'a> BitBuf<'a> {
         } else {
             self.data = self.data.get(bits / 8..).ok_or(Insufficient)?;
         }
+        self.len += bits;
         Ok(())
     }
 
-    fn data_at_offset(&self, offset: usize, size: usize) -> Option<u8> {
-        let len = self.len();
-        let offset = self.prefix as usize + offset;
-        if offset == 0 {
-            if len == 0 {
-                return None;
-            }
-            Some(self.data[0])
-        } else if len < size {
-            None
-        } else {
-            let offset_bytes = offset / 8;
-            let offset_rem = offset & 7;
-            if offset_rem == 0 {
-                Some(self.data[offset_bytes])
-            } else {
-                let offset_rem_inv = 8 - offset_rem;
-                Some(if size + offset_rem <= 8 {
-                    ((self.data[offset_bytes] & (255 >> offset_rem)) << offset_rem)
-                } else {
-                    ((self.data[offset_bytes] & (255 >> offset_rem)) << offset_rem)
-                        + ((self.data[(offset_bytes) + 1] & (255 << offset_rem_inv))
-                            >> offset_rem_inv)
-                })
-            }
-        }
-    }
-
-    pub fn byte_at_offset(&self, offset: usize) -> Option<u8> {
-        self.data_at_offset(offset, 8)
-    }
-
-    pub fn copy_aligned(&mut self, dst: &mut [u8]) -> Result<(), Insufficient> {
+    fn copy_aligned(&mut self, dst: &mut [u8]) -> Result<(), CopyError> {
         Ok(for i in 0..dst.len() {
             dst[i] = self.byte_at_offset(i * 8).ok_or(Insufficient)?;
         })
     }
 
-    pub fn copy_to_slice(&mut self, dst: &mut [u8], bits: usize) -> Result<(), CopyError> {
+    fn len(&self) -> usize {
+        self.len
+    }
+
+    fn copy_to_slice(&mut self, dst: &mut [u8], bits: usize) -> Result<(), CopyError> {
         let bytes = bits / 8;
         let len = dst.len();
         if len < bytes {
@@ -102,44 +88,91 @@ impl<'a> BitBuf<'a> {
         Ok(())
     }
 
-    pub fn pop(&mut self) -> Option<bool> {
+    fn pop(&mut self) -> Option<bool> {
         let byte = self.byte_at_offset(0)?;
         self.advance(1).unwrap();
         Some(byte & 128 != 0)
     }
 
-    pub fn pop_byte(&mut self) -> Option<u8> {
+    fn pop_byte(&mut self) -> Option<u8> {
         let byte = self.byte_at_offset(0)?;
         self.advance(8).unwrap();
         Some(byte)
     }
 
-    pub fn len(&self) -> usize {
+    fn remaining(&self) -> usize {
         self.data.len() * 8 - self.prefix as usize
     }
 }
 
-#[derive(Debug)]
-pub struct BitBufMut<'a> {
-    data: &'a mut [u8],
-    prefix: u8,
-    len: usize,
-}
-
-impl<'a> BitBufMut<'a> {
-    pub fn new(data: &'a mut [u8]) -> Self {
-        BitBufMut {
+impl<'a> BitSlice<'a> {
+    pub fn new(data: &'a [u8]) -> Self {
+        BitSlice {
             data,
             prefix: 0,
             len: 0,
         }
     }
 
-    pub fn len(&self) -> usize {
+    fn data_at_offset(&self, offset: usize, size: usize) -> Option<u8> {
+        let len = self.remaining();
+        let offset = self.prefix as usize + offset;
+        if offset == 0 {
+            if len == 0 {
+                return None;
+            }
+            Some(self.data[0])
+        } else if len < size {
+            None
+        } else {
+            let offset_bytes = offset / 8;
+            let offset_rem = offset & 7;
+            if offset_rem == 0 {
+                Some(self.data[offset_bytes])
+            } else {
+                let offset_rem_inv = 8 - offset_rem;
+                Some(if size + offset_rem <= 8 {
+                    ((self.data[offset_bytes] & (255 >> offset_rem)) << offset_rem)
+                } else {
+                    ((self.data[offset_bytes] & (255 >> offset_rem)) << offset_rem)
+                        + ((self.data[(offset_bytes) + 1] & (255 << offset_rem_inv))
+                            >> offset_rem_inv)
+                })
+            }
+        }
+    }
+
+    fn byte_at_offset(&self, offset: usize) -> Option<u8> {
+        self.data_at_offset(offset, 8)
+    }
+}
+
+#[derive(Debug)]
+pub struct BitSliceMut<'a> {
+    data: &'a mut [u8],
+    prefix: u8,
+    len: usize,
+}
+
+pub trait BitBufMut {
+    fn len(&self) -> usize;
+    fn advance(&mut self, bits: usize) -> Result<(), Insufficient>;
+    fn push(&mut self, item: bool) -> Result<(), Insufficient>;
+    fn put(&mut self, data: &[u8], bits: usize) -> Result<(), CopyError>;
+    fn put_byte(&mut self, data: u8) -> Result<(), CopyError> {
+        self.put_aligned(&[data])
+    }
+    fn put_aligned(&mut self, data: &[u8]) -> Result<(), CopyError> {
+        self.put(data, data.len() * 8)
+    }
+}
+
+impl<'a> BitBufMut for BitSliceMut<'a> {
+    fn len(&self) -> usize {
         self.len
     }
 
-    pub fn advance(&mut self, bits: usize) -> Result<(), Insufficient> {
+    fn advance(&mut self, bits: usize) -> Result<(), Insufficient> {
         self.prefix += (bits & 7) as u8;
         if self.prefix >= 8 {
             self.prefix -= 8;
@@ -155,7 +188,7 @@ impl<'a> BitBufMut<'a> {
         Ok(())
     }
 
-    pub fn push(&mut self, item: bool) -> Result<(), Insufficient> {
+    fn push(&mut self, item: bool) -> Result<(), Insufficient> {
         if self.data.len() == 0 {
             return Err(Insufficient);
         }
@@ -169,36 +202,15 @@ impl<'a> BitBufMut<'a> {
         Ok(())
     }
 
-    fn write(&mut self, item: u8, bits: usize) -> Result<(), Insufficient> {
-        if self.prefix == 0 {
-            let inv_bits = 8 - bits;
-            let litem = item & (255 << (inv_bits));
-            let hitem = item | (255 >> (inv_bits));
-            self.data[0] |= litem;
-            self.data[0] &= hitem;
-        } else {
-            let inv_bits = 8 - bits;
-            let inv_prefix = 8 - self.prefix;
-            let litem = item & (255 << inv_bits);
-            let hitem = item | (255 >> inv_bits);
-            self.data[0] |= litem >> self.prefix;
-            self.data[0] &= (hitem >> self.prefix) | (255 << inv_prefix);
-            self.data[1] |= litem << inv_prefix;
-            self.data[1] &= (hitem << inv_prefix) | (255 << self.prefix);
-        }
-        self.advance(bits)?;
-        Ok(())
-    }
-
-    pub fn put_byte(&mut self, item: u8) -> Result<(), Insufficient> {
+    fn put_byte(&mut self, item: u8) -> Result<(), CopyError> {
         if self.data.len() == 0 {
-            return Err(Insufficient);
+            return Err(Insufficient.into());
         }
         if self.prefix == 0 {
             self.data[0] = item;
         } else {
             if self.data.len() == 1 {
-                return Err(Insufficient);
+                return Err(Insufficient.into());
             }
             let inv_prefix = 8 - self.prefix;
             self.data[0] |= item >> self.prefix;
@@ -210,7 +222,7 @@ impl<'a> BitBufMut<'a> {
         Ok(())
     }
 
-    pub fn put(&mut self, data: &[u8], bits: usize) -> Result<(), CopyError> {
+    fn put(&mut self, data: &[u8], bits: usize) -> Result<(), CopyError> {
         let bytes = bits / 8;
         if bits == 0 {
             return Ok(());
@@ -236,10 +248,41 @@ impl<'a> BitBufMut<'a> {
         Ok(())
     }
 
-    pub fn put_aligned(&mut self, data: &[u8]) -> Result<(), Insufficient> {
+    fn put_aligned(&mut self, data: &[u8]) -> Result<(), CopyError> {
         for byte in data {
             self.put_byte(*byte)?;
         }
+        Ok(())
+    }
+}
+
+impl<'a> BitSliceMut<'a> {
+    pub fn new(data: &'a mut [u8]) -> Self {
+        BitSliceMut {
+            data,
+            prefix: 0,
+            len: 0,
+        }
+    }
+
+    fn write(&mut self, item: u8, bits: usize) -> Result<(), Insufficient> {
+        if self.prefix == 0 {
+            let inv_bits = 8 - bits;
+            let litem = item & (255 << (inv_bits));
+            let hitem = item | (255 >> (inv_bits));
+            self.data[0] |= litem;
+            self.data[0] &= hitem;
+        } else {
+            let inv_bits = 8 - bits;
+            let inv_prefix = 8 - self.prefix;
+            let litem = item & (255 << inv_bits);
+            let hitem = item | (255 >> inv_bits);
+            self.data[0] |= litem >> self.prefix;
+            self.data[0] &= (hitem >> self.prefix) | (255 << inv_prefix);
+            self.data[1] |= litem << inv_prefix;
+            self.data[1] &= (hitem << inv_prefix) | (255 << self.prefix);
+        }
+        self.advance(bits)?;
         Ok(())
     }
 }
